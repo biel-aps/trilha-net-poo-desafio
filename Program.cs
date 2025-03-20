bbddaaba88e9852d3a340f17491cdaa2ceec3d1a
Gabriel Alves <g.alvespds@gmail.com>
2025-03-20 19:57:10 -0300

    Desafio finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// implementado: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone 15:");
+Iphone ip = new Iphone("(19)12345678","Iphone 15","12535525258282",16);
+ip.Ligar();
+ip.InstalarAplicativo("Facebook");
+ip.ReceberLigacao();
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nk =new Nokia("(19)987654321","Nokia 2025","123574785784525",16);
+nk.InstalarAplicativo("Mercado Livre");
+nk.Ligar();
+nk.ReceberLigacao();
